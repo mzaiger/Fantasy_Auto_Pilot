7befe3255743f77b3a954d24201d98bef69c6b8c
Marc Zaiger <mzaiger@gmail.com>
2026-08-21 03:57:44 -0500

    Update CookiesConvertToTextForSecret.ps1

--- CookiesConvertToTextForSecret.ps1
+++ CookiesConvertToTextForSecret.ps1
@@ -1,2 +1,6 @@
 # Windows PowerShell
-[Convert]::ToBase64String([IO.File]::ReadAllBytes("yahoo_cookies.json")) | Set-Content -NoNewline -Encoding ascii yahoo_cookies_b64.txt
+$folder = "C:\"
+
+[Convert]::ToBase64String(
+    [IO.File]::ReadAllBytes("$folder\yahoo_cookies.json")
+) | Set-Content -NoNewline -Encoding ascii "$folder\yahoo_cookies_b64.txt"
